0767b7823f9429784057dd941f69180f87717119
Abdukhaligov <hikmat.pou@gmail.com>
2022-03-26 22:45:15 +0400

    add: auth.server guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
       'driver' => 'session',
       'provider' => 'users',
     ],
+
+    'server' => [
+      'driver' => 'json',
+      'provider' => 'users',
+    ],
   ],
 
   /*
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('welcome');
+Route::middleware('auth.server')->get('/', function (){
+  return Auth::guard('server')->user();
 });
