77ffd1f5bb08c7df7d1a3ade0997dceb370ffd19
Abdukhaligov <hikmat.pou@gmail.com>
2024-02-02 04:25:21 +0400

    update: composer packages & overwrite db:wipe command

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,9 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable {
-  use HasApiTokens, Notifiable;
+  use Notifiable;
 
   /**
    * The attributes that are mass assignable.
